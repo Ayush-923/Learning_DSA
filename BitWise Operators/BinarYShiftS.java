7bc07503fcf106d89a01c3a65dcc7d63a0ba8861
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2022-12-09 00:33:46 +0530

    Add files via upload

---
+++ BitWise Operators/BinarYShiftS.java
@@ -0,0 +1,18 @@
+public class BinarYShiftS {
+   
+    public static void main(String[] args) {
+        //BINARRY LEFT SHIFT OPERATOR
+        //FOR A << B(2)
+        //IT WILL SHIFT THE BY 2 VALUES TOWARD LEFT
+        // A << B =  A * 2^B 
+        System.out.println(5<<2);
+        
+
+
+        //BINARRY RIGHT SHIFT
+        //FOR A>>B = A / 2^B
+        // 6 >> 1 = 6 / 2^1 = 6/2 = 3
+        System.out.println(6 >> 1);
+
+    }
+}
