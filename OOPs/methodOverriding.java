6937dbe813c5ea08456ca554d270f49273e16e64
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2022-12-14 22:12:25 +0530

    Add files via upload

---
+++ OOPs/methodOverriding.java
@@ -0,0 +1,18 @@
+public class methodOverriding {
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eat anythings.");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats Grass");
+    }
+}
