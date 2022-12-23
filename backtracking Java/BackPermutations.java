b097e81e9d15374b7fd6641cd2d8921eb5872832
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2022-12-23 23:35:57 +0530

    Add files via upload

---
+++ backtracking Java/BackPermutations.java
@@ -0,0 +1,25 @@
+public class BackPermutations {
+
+    public static void printPeermutation(String str, String ans)
+    {
+        //base case
+        if(str.length() == 0)
+        {
+            System.out.println(ans);
+            return;
+        }
+        //kaam
+        for(int i=0; i<str.length(); i++)
+        {
+            char curr = str.charAt(i);
+            //"abcde" = "ab" + "de" = "abde"
+            String newstr = str.substring(0, i) + str.substring(i+1);
+            printPeermutation(newstr, ans+curr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPeermutation(str, " ");
+    }
+}
